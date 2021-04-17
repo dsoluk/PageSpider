975bf32d57a1f35666a56bceeb855eb1c4531038
dsoluk <70490492+dsoluk@users.noreply.github.com>
2021-04-16 21:06:34 -0400

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path: str):
+    #TODO add code to create database
+    pass
+
+def save_words_to_database(database_path: str, words_list: list):
+    #TODO add code to save words to db
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(file_path:str):
+    #TODO: add code needed to read the text file of URLs
+    pass
+
+def load_url (url: str):
+    #TODO: add code for reading URL from text file
+    pass
+
+def scrape_page (page_contents: str):
+    #TODO: add code to read page contents
+    pass
+
